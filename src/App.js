81a0fae1de80cd33c2f913d58923d77cf79467f4
Ayush <ayushkumarsingh525@gmail.com>
2022-02-18 17:29:38 +0530

    Fina commit

--- src/App.js
+++ src/App.js
@@ -10,24 +10,36 @@ const DUMMY_EXPENSES = [
     amount: 500,
     date: new Date(2020, 7, 14),
   },
-  { id: 'e2', title: 'New TV', amount: 8000, date: new Date(2021, 2, 12) },
+  { id: 'e2', title: 'New TV', amount: 8000, date: new Date(2022, 2, 12) },
   {
     id: 'e3',
     title: 'Car Insurance',
     amount: 4000,
-    date: new Date(2021, 2, 28),
+    date: new Date(2022, 1, 28),
   },
   {
     id: 'e4',
     title: 'Desk (Wooden)',
     amount: 18000,
-    date: new Date(2021, 5, 12),
+    date: new Date(2020, 5, 12),
   },
   {
     id: 'e5',
     title: 'Chair (Wooden)',
     amount: 2000,
-    date: new Date(2021, 5, 14),
+    date: new Date(2020, 5, 14),
+  },
+  {
+    id: 'e6',
+    title: 'Car',
+    amount: 2000000,
+    date: new Date(2019, 11, 4),
+  },
+  {
+    id: 'e7',
+    title: 'Headphones',
+    amount: 2000,
+    date: new Date(2019, 5, 14),
   }
 ];
 
@@ -36,9 +48,11 @@ const App = () => {
 
   const addExpenseHandler = (expense) => {
     setExpenses((prevExpenses) => {
-      return [...prevExpenses, expense];
+      return [expense, ...prevExpenses];
     });
   };
+
+
   return (
     <div>
       <NewExpense onAddExpense={addExpenseHandler} />
